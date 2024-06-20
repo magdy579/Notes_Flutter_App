027d729834db75134ab5d96b3afa74eb8caa55c6
unknown <magdyah319@gmail.com>
2024-06-20 03:42:24 +0300

    Create note app

---
+++ lib/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeNote extends StatelessWidget {
+  const HomeNote({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
