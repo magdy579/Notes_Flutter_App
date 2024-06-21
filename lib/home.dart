42477f9832922f57f20855b82775c6632a4c170f
kh <magdyah319@gmail.com>
2024-06-21 04:58:07 +0300

    Create note app

--- lib/home.dart
+++ lib/home.dart
@@ -6,7 +6,56 @@ class HomeNote extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+       body:
+       Padding(
+         padding: const EdgeInsets.symmetric(horizontal: 24.0),
+         child: Column(
+           children: [
+             SizedBox(height: 20),
+             customAppbar(),
+           ],
+         ),
+       ),
+    );
+  }
+
+}
+class customAppbar extends StatelessWidget {
+  const customAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+  Text('Notes ',style: TextStyle(
+  fontSize: 25,
+  fontWeight: FontWeight.w400,
+  ),),
+  Spacer(),
+  CustomSearchIcon(),
+
+  ],
+  );
+}
+}
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      height: 45,
+      width: 45,
+
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(.05),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Center(
+        child: Icon(Icons.search_outlined),
+      ),
     );
   }
 }
+
+
