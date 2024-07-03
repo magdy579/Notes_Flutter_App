cab5e3a12f8d7a233e45af5de7fb49dfb8e3e301
kh <magdyah319@gmail.com>
2024-07-03 03:01:06 +0300

    add custom text form field

--- lib/Widget/CustomTectField.dart
+++ lib/Widget/CustomTectField.dart
@@ -3,12 +3,22 @@ import 'package:flutter/material.dart';
 import '../const.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hint, this.maxLine=1});
+  const CustomTextField({super.key, required this.hint, this.maxLine=1, this.onSaved});
 final String hint;
 final int maxLine;
+final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onSaved,
+validator: (value){
+        if(value?.isEmpty ?? true){
+
+          return 'Field is Empty ';
+        }else{
+          return null;
+        }
+},
 maxLines: maxLine,
       cursorColor: kColor,
       decoration:InputDecoration(
--- lib/Widget/showBottomSheet.dart
+++ lib/Widget/showBottomSheet.dart
@@ -9,44 +9,82 @@ class bottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      padding:  EdgeInsets.symmetric(horizontal: 16.0),
       child: SingleChildScrollView(
         child:  Container(
           height: 600,
-          child: Column(
-              children: [
-                SizedBox(
-                  height: 32,
-                ),
-                CustomTextField(
-                  hint: 'title',
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                CustomTextField(
-                  hint: 'Enter Note',
-                  maxLine: 5,
-                ),
-                SizedBox(
-                  height: 32,
-                ),
-                ElevatedButton(
-
-                    style: ElevatedButton.styleFrom(
-                      maximumSize: const Size(double.infinity, 46),
-                      minimumSize: const Size(double.infinity, 46),
-                      backgroundColor:kColor,
-                    ),
-                    onPressed: (){}, child: const Text('Add',style: TextStyle(
-                  color: Colors.black
-                ),)),
-                SizedBox(
-                  height: 32,
-                ),
-              ],
-          ),
+          child: const add_note_form(),
         ),
+      ),
+    );
+  }
+}
+
+class add_note_form extends StatefulWidget {
+  const add_note_form({
+    super.key,
+  });
+
+  @override
+  State<add_note_form> createState() => _add_note_formState();
+}
+
+class _add_note_formState extends State<add_note_form> {
+
+  final GlobalKey<FormState> formkey =GlobalKey();
+  AutovalidateMode autovalidateMode =AutovalidateMode.disabled;
+  String? title,subTitle;
+  Widget build(BuildContext context) {
+    return Form(
+      key: formkey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+          children: [
+            const SizedBox(
+              height: 32,
+            ),
+             CustomTextField(
+               onSaved: (value){
+                 title =value;
+               },
+              hint: 'title',
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+             CustomTextField(
+               onSaved: (value){
+                 subTitle =value;
+               },
+              hint: 'Enter Note',
+              maxLine: 5,
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            ElevatedButton(
+
+                style: ElevatedButton.styleFrom(
+                  maximumSize: const Size(double.infinity, 46),
+                  minimumSize: const Size(double.infinity, 46),
+                  backgroundColor:kColor,
+                ),
+                onPressed: (){
+                  if(formkey.currentState!.validate()){
+                    formkey.currentState!.save();
+                  }else{
+                    autovalidateMode =AutovalidateMode.always;
+                    setState(() {
+
+                    });
+                  }
+                }, child: const Text('Add',style: TextStyle(
+              color: Colors.black
+            ),)),
+            const SizedBox(
+              height: 32,
+            ),
+          ],
       ),
     );
   }
