d9fd7e51193d150ff718453d91ca7a44cede0d0a
kh <magdyah319@gmail.com>
2024-07-06 05:26:57 +0300

    refactor bloc provider

--- lib/Widget/showBottomSheet.dart
+++ lib/Widget/showBottomSheet.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
@@ -8,38 +7,41 @@ import 'package:notesapp/const.dart';
 import 'package:notesapp/cubits/add_note_cubit.dart';
 
 class bottomSheet extends StatefulWidget {
-   const bottomSheet({super.key});
+  const bottomSheet({super.key});
 
   @override
   State<bottomSheet> createState() => _bottomSheetState();
 }
 
 class _bottomSheetState extends State<bottomSheet> {
-bool isLoading =false;
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 16.0),
-      child: BlocConsumer<AddNoteCubit,AddNoteState>(
-        listener: (context, state) {
-          if(state is AddNoteLoading){
-            isLoading =true;
-          }
-          if(state is AddNoteFailure){
-            print('Failed ${state.errMessage}');
-          }
-          if(state is AddNoteSuccess){
-            Navigator.pop(context);
-          }
-        },
+    return BlocProvider(
+      create: (context) => AddNoteCubit(),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteLoading) {
+              isLoading = true;
+            }
+            if (state is AddNoteFailure) {
+              print('Failed ${state.errMessage}');
+            }
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            }
+          },
           builder: (context, state) {
             return ModalProgressHUD(
-                inAsyncCall:isLoading,
+                inAsyncCall: isLoading,
                 child: const SingleChildScrollView(
                     child: add_note_form()));
           },
 
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -22,23 +22,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers:[
-        BlocProvider(
-          create: (BuildContext context) => AddNoteCubit(),
-        )
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          brightness: Brightness.dark,
-          fontFamily: 'Poppins',
-        ),
-        home:  HomeNote(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: 'Poppins',
+      ),
+      home:  HomeNote(),
 routes: {
   EditNotes.id :(context)=> const EditNotes(),
 },
-      ),
     );
   }
 }
